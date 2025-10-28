d66b7c2edbde97eb5171135af3a4a881aeb3ed06
opilane <opilane@HP-CZC2349HV2>
2025-10-28 13:19:27 +0200

    test2

---
+++ Controllers/PhotoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace veebipood.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public PhotoController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Photo>>> GetPhotos()
+        {
+            var response = await _httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/photos");
+            var photos = JsonConvert.DeserializeObject<List<Photo>>(response);
+
+            return Ok(photos);
+        }
+    }
+
+    public class Photo
+    {
+        public int AlbumId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
+
